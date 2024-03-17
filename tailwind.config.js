e24339def6562019bc7ba633d933e878e7fff68a
ABD <abd@example.com>
2024-03-17 11:35:40 +0530

    Completed UI Portion

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./Main/*.html', './Main/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'custom': '3px 8px 15px 3px rgba(0, 0, 0, 0.1), 0 -2px 6px -4px rgba(0, 0, 0, 0.1)',
+      },
+      textDecoder: {
+        'underline': 'underline',
+      },
+    },
   },
   plugins: [],
 }
